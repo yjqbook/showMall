b8ab5d2a47a297db9f46499b76413209b9b01310
Administrator <1095737364@qq.cpm>
2020-12-08 22:50:25 +0800

    更改连接

--- mall/src/main/java/com/yys/mall/controller/UserController.java
+++ mall/src/main/java/com/yys/mall/controller/UserController.java
@@ -3,6 +3,7 @@ package com.yys.mall.controller;
 import com.yys.mall.entity.DbUser;
 import com.yys.mall.service.DbUserService;
 import com.yys.mall.utils.ResultUtil;
+import com.yys.mall.utils.Sms;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,63 +22,86 @@ public class UserController {
 
     /**
      * 添加user
+     *
      * @param request
      * @return
      */
     @RequestMapping("/addUser")
     @ResponseBody
-    public ResultUtil addUser(String name,String birthday,String phone,String email, HttpServletRequest request) throws Exception{
+    public ResultUtil addUser(String name, String birthday, String phone, String email, String code, HttpServletRequest request) throws Exception {
+        String codeMy=String.valueOf(request.getSession().getAttribute("code"));
+        if(codeMy!=null && !codeMy.equals(code)){
+            return ResultUtil.error("验证码不正确!");
+        }
 
-        DbUser user=new DbUser();
+        DbUser user = new DbUser();
         user.setName(name);
         user.setPhone(phone);
         user.setEmail(email);
-        user.setBirthday( new SimpleDateFormat("yyyy-MM-dd").parse(birthday));
+        user.setBirthday(new SimpleDateFormat("yyyy-MM-dd").parse(birthday));
         user.setCreateTime(new Date());
         int num = userService.addUser(user);
-        if(num > 0){
+        if (num > 0) {
+            request.getSession().setAttribute("code", 0);
             return ResultUtil.success("添加成功!");
-        }else{
+        } else {
             return ResultUtil.error("添加失败!");
         }
     }
 
     /**
+     * 发送验证码
+     *
+     * @param request
+     * @return
+     */
+    @RequestMapping("/sendCode")
+    @ResponseBody
+    public ResultUtil sendCode(String phone, HttpServletRequest request) throws Exception {
+        int code = (int) ((Math.random() * 9 + 1) * 100000);
+        request.getSession().setAttribute("code", code);
+        String sendReturn = Sms.send(phone, String.valueOf(code));
+        if (sendReturn != null) {
+            return ResultUtil.success("发送成功!");
+        } else {
+            return ResultUtil.error("发送失败!");
+        }
+    }
+
+
+    /**
      * 删除user
+     *
      * @param id
      * @param request
      * @return
      */
     @RequestMapping("/deleteUser")
     @ResponseBody
-    public ResultUtil deleteUser(Integer id , HttpServletRequest request) {
+    public ResultUtil deleteUser(Integer id, HttpServletRequest request) {
         int num = userService.deleteUser(id);
-        if(num > 0){
+        if (num > 0) {
             return ResultUtil.success("删除成功!");
-        }else{
+        } else {
             return ResultUtil.error("删除失败!");
         }
     }
 
 
-
-
     /**
      * 查询user 列表
+     *
      * @param page
      * @param limit
      * @param request
      * @return
      */
-    @RequestMapping(value = "getUserList",method = {RequestMethod.GET,RequestMethod.POST})
+    @RequestMapping(value = "getUserList", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public ResultUtil getUserList(Integer page, Integer limit,HttpServletRequest request){
+    public ResultUtil getUserList(Integer page, Integer limit, HttpServletRequest request) {
 
-        return userService.getUserList(page,limit);
+        return userService.getUserList(page, limit);
     }
-
-
-
 
 
 }
---
+++ mall/src/main/java/com/yys/mall/utils/Sms.java
@@ -0,0 +1,110 @@
+package com.yys.mall.utils;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Sms {
+    public static String HTTPPost(String sendUrl, String sendParam) {
+        String codingType = "UTF-8";
+        StringBuffer receive = new StringBuffer();
+        BufferedWriter wr = null;
+        try {
+            // 建立连接
+            URL url = new URL(sendUrl);
+            HttpURLConnection URLConn = (HttpURLConnection) url.openConnection();
+            URLConn.setDoOutput(true);
+            URLConn.setDoInput(true);
+            ((HttpURLConnection) URLConn).setRequestMethod("POST");
+            URLConn.setUseCaches(false);
+            URLConn.setAllowUserInteraction(true);
+            HttpURLConnection.setFollowRedirects(true);
+            URLConn.setInstanceFollowRedirects(true);
+            URLConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+            URLConn.connect();
+
+            DataOutputStream dos = new DataOutputStream(URLConn.getOutputStream());
+            dos.writeBytes(sendParam);
+            BufferedReader rd = new BufferedReader(new InputStreamReader(
+                    URLConn.getInputStream(), codingType));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                receive.append(line).append("\r\n");
+            }
+            rd.close();
+        } catch (IOException e) {
+            receive.append("访问产生了异常-->").append(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            if (wr != null) {
+                try {
+                    wr.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                wr = null;
+            }
+        }
+        return receive.toString();
+    }
+
+    // 发送短信
+    private static String sendMessage(String sendUrl, String userid, String account,
+                                      String password, String mobile, String content) {
+        String codingType = "UTF-8";
+        StringBuffer sendParam = new StringBuffer();
+        try {
+            // sendParam.append("action=").append("send");
+            sendParam.append("&userid=").append(userid);
+            sendParam.append("&account=").append(URLEncoder.encode(account, codingType));
+            sendParam.append("&password=").append(URLEncoder.encode(password, codingType));
+            sendParam.append("&mobile=").append(mobile);
+            sendParam.append("&content=").append(URLEncoder.encode(content, codingType));
+        } catch (Exception e) {
+            // 处理异常
+            e.printStackTrace();
+        }
+        return Sms.HTTPPost(sendUrl, sendParam.toString());
+    }
+
+    // 查询余额
+    public static String Overage(String sendUrl, String userid, String account,
+                                 String password) {
+        String codingType = "UTF-8";
+        StringBuffer sendParam = new StringBuffer();
+        try {
+            sendParam.append("action=").append("overage");
+            sendParam.append("&userid=").append(userid);
+            sendParam.append("&account=").append(URLEncoder.encode(account, codingType));
+            sendParam.append("&password=").append(URLEncoder.encode(password, codingType));
+        } catch (Exception e) {
+            // 处理异常
+            e.printStackTrace();
+        }
+        return Sms.HTTPPost(sendUrl, sendParam.toString());
+    }
+
+    public static String url = "http://hyt.uewang.net/SmsWebService.asmx/SendSms"; // 对应平台地址
+    public static String userid = "0001"; // 客户id
+    public static String account = "xxxx"; // 账号
+    public static String password = "123456"; // 密码
+    public static String mobile = "13000000000"; // 手机号码，多个号码使用","分割
+    public static String content = "欢迎加入cpe会员！您的验证码是:"; // 短信内容
+
+
+    public static String send(String phone,String code) {
+        String sendReturn = Sms.sendMessage(url, userid, account, password, phone, content+code);
+        return sendReturn;
+    }
+
+    public static void main(String[] args) {
+        // 发送短信
+        String sendReturn = Sms.sendMessage(url, userid, account, password, mobile, content);
+        System.out.println(sendReturn); // 处理返回值,参见HTTP协议文档
+    }
+}
